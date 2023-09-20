8bb2c41f30f97007ae51369322471b60602a64ee
Bharat <bharatlakhera295@gmail.com>
2023-09-20 21:18:39 +0530

    add

--- database/db.js
+++ database/db.js
@@ -4,7 +4,7 @@ const Connection = async (username, password) => {
     // const URL = `mongodb://localhost:27017/temp`;
     
     const URL = `mongodb+srv://${username}:${password}@cluster0.o1byfzn.mongodb.net/?retryWrites=true&w=majority`;
-      
+
     try {
         await mongoose.connect(URL, { useNewUrlParser: true })
         console.log('Database connected successfully');
